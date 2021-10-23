797139cd002c1a35ef3dbbf01ae5ba4cb953987a
AlexKyriacou <a.kyriacou14@gmail.com>
2021-10-23 17:25:32 +1100

    Solution Upload

---
+++ Entity Component System/Components/ECS_CTransform.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResearchProject
+{
+    public class ECS_CTransform : ECS_Component
+    {
+        public int x, y;
+    }
+}
